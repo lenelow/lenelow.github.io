6f1f77326d4f813c5a96d7f85fdce815cda09fa3
Leah Enelow <lenelow@students.terpmail.edu>
2018-07-28 20:49:30 -0400

    smooth transitions

--- portfolio.js
+++ portfolio.js
@@ -36,3 +36,45 @@ function typewriter() {
 }
 
 typewriter();
+
+// Select all links with hashes
+$('a[href="#invisibleP"]')
+  // Remove links that don't actually link to anything
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length
+        ? target
+        : $("[name=button]" + this.hash.slice(1) + "]");
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top
+          },
+          1000,
+          function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) {
+              // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            }
+          }
+        );
+      }
+    }
+  });
