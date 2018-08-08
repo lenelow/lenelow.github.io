e6264d21b0b36b14285e33279696b4a10c6070ed
Leah Enelow <lenelow@students.terpmail.edu>
2018-08-08 18:48:48 -0400

    making container two responsive + other details

--- portfolio.js
+++ portfolio.js
@@ -135,6 +135,48 @@ $('a[href="#secondInvisibleP"]')
     }
   });
 
+// Select all links with hashes
+$('a[href="#Three"]')
+  // Remove links that don't actually link to anything
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length
+        ? target
+        : $("[name=img]" + this.hash.slice(1) + "]");
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top
+          },
+          800,
+          function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) {
+              // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            }
+          }
+        );
+      }
+    }
+  });
+
 function show(idOne, idTwo, idThree, idFour) {
   document.getElementById(idOne).style.visibility = "visible";
   document.getElementById(idTwo).style.visibility = "visible";
