b7038f25d78734b85ea677bba8ad9c165eed50e5
Leah Enelow <lenelow@students.terpmail.edu>
2018-08-08 16:09:25 -0400

    photo

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,18 @@
+window.onscroll = function() {
+  stickyFunction();
+};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickyFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
 // set up text to print, each item in array is new line
 var aText = new Array(
   "Hi there!",
