dc9afc5ea7ddc1655590f5ecffba30c471d67d36
Leah Enelow <lenelow@students.terpmail.edu>
2018-08-13 00:25:10 -0400

    fixed button issue

--- portfolio.js
+++ portfolio.js
@@ -17,7 +17,7 @@ function stickyFunction() {
 var aText = new Array(
   "Hi there!",
   "My name is Leah.",
-  "I'm a full-stack developer,",
+  "I'm a web developer,",
   "and I'm itching to learn and build new things."
 );
 var iSpeed = 20; // time delay of print out
